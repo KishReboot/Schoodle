1b14cc03da36dd00ff0b131ad68a9c2afbffd247
KishReboot <russellsalopek@pm.me>
2023-01-19 14:23:40 -0500

    Add new event page for showing details, and add post to show that.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser')
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -16,6 +17,8 @@ app.set('view engine', 'ejs');
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 app.use(
   '/styles',
   sassMiddleware({
@@ -28,25 +31,42 @@ app.use(express.static('public'));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const userApiRoutes = require('./routes/users-api');
-
-const usersRoutes = require('./routes/users');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
-app.use('/api/users', userApiRoutes);
+//app.use('/api/users', userApiRoutes);
 
-app.use('/users', usersRoutes);
+//app.use('/users', usersRoutes);
 // Note: mount other resources here, using the same pattern above
 
-// Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
+
+// Event creation page 
 app.get('/', (req, res) => {
+  
   res.render('index');
+
 });
+
+// Event info 
+app.post('/', (req, res) => {
+  const {
+    eventTitle, eventDesc, organizerName, organizerEmail, eventDate, eventTime
+  } = req.body;
+  res.render('event', {
+    title: eventTitle, description: eventDesc, name: organizerName, email: organizerEmail, date: eventDate, time: eventTime})
+});
+
+app.post('/', (req, res)=>{
+  const {
+    eventTitle, eventDesc, organizerName, organizerEmail, eventDate, eventTime
+  } = req.body;
+  console.log(req.body);
+  res.json({title: eventTitle, description: eventDesc, name: organizerName, email: organizerEmail, date: eventDate, time: eventTime})
+})
 
 app.listen(PORT, () => {
   console.log(`Schoodle app listening on port ${PORT}`);
